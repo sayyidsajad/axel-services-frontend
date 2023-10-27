8e23af823f2aca2f8c8d48f157a0ff29488b17da
Sayyid Sajad <sayyidsajada4s@gmail.com>
2023-10-27 12:13:30 +0530

    Untrack src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    APIURL: 'http://localhost:3000/',
-    userSecret:'userSecret',
-    adminSecret:'adminSecret',
-    servicerSecret:'servicerSecret',
-    razorKey:'rzp_test_G1BmZFRY7zNMXT',
-    razorEmail:'sayyidsajada4s@gmail.com',
-    razorPhone:'9074649087'
-};
