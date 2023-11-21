f2b5c3e85e41714e746c85d64e5f6858bbdbb2d5
sayyidsajad <sayyidsajada4s@gmail.com>
2023-11-21 18:53:08 +0530

    build done

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    socket_endPoint:'http://localhost:3000',
+    APIURL:'http://localhost:3000/',
+    servicerSecret:'servicerSecret',
+    adminSecret:'adminSecret',
+    userSecret:'userSecret',
+    razorPhone:'9074649087',
+    razorEmail:'sayyidsajada4s@gmail.com',
+    razorKey:'rzp_test_G1BmZFRY7zNMXT',
+    mapApiKey:'AIzaSyDQF4CxmfbJg0HUURmVGfCX0NR6sSbGsOo',
+    googleProvider:'916587595257-8fqlt0henbsrp739bo6j2kpsh37rm9nb.apps.googleusercontent.com'
+  };
+  
