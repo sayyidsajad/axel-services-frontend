664b49d0f02aa61cc62df7ac32e36f3405eb148b
sayyidsajad <sayyidsajada4s@gmail.com>
2023-11-30 20:32:27 +0530

    env update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  socketEndPoint: 'http://localhost:3000',
-  apiUrl: 'http://localhost:3000/',
+  socketEndPoint: 'https://axelservices.online',
+  apiUrl: 'https://axelservices.online/',
   servicerSecret: 'servicerSecret',
   adminSecret: 'adminSecret',
   userSecret: 'userSecret',
