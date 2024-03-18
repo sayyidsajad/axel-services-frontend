bbd02a8c14135ae70857fcf158ae327841f9d2cb
Sayyid Sajad <sayyidsajada4s@gmail.com>
2024-03-18 14:59:55 +0530

    adding environments in git

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  socketEndPointLocal: 'http://localhost:3000',
-  socketEndPoint: 'http://localhost:3000',
-  apiUrl: 'http://localhost:3000/',
+  socketEndPointLocal: 'https://server.axelservices.online',
+  socketEndPoint: 'https://server.axelservice.online',
+  apiUrl: 'https://server.axelservices.online/',
   servicerSecret: 'servicerSecret',
   adminSecret: 'adminSecret',
   userSecret: 'userSecret',
@@ -12,5 +12,5 @@ export const environment = {
   mapApiKey: 'AIzaSyDQF4CxmfbJg0HUURmVGfCX0NR6sSbGsOo',
   googleProvider: '916587595257-8fqlt0henbsrp739bo6j2kpsh37rm9nb.apps.googleusercontent.com',
   googleMapScript: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDQF4CxmfbJg0HUURmVGfCX0NR6sSbGsOo&libraries=places&callback=initMap&solution_channel=GMP_QB_addressselection_v1_cABC',
-  recaptcha:'6LePsSYpAAAAAPMwLtvMEJ_7ove_uFa3zSEt0YDw'
+  recaptcha: '6LePsSYpAAAAAPMwLtvMEJ_7ove_uFa3zSEt0YDw'
 };
