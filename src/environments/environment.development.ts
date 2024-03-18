325c039b4035160ba191c9543af512381e4d496f
Sayyid Sajad <sayyidsajada4s@gmail.com>
2024-03-18 14:55:27 +0530

    adding environments in git

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  socketEndPointLocal: 'http://localhost:3000',
+  socketEndPoint: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000/',
+  servicerSecret: 'servicerSecret',
+  adminSecret: 'adminSecret',
+  userSecret: 'userSecret',
+  razorPhone: '9074649087',
+  razorEmail: 'sayyidsajada4s@gmail.com',
+  razorKey: 'rzp_test_G1BmZFRY7zNMXT',
+  mapApiKey: 'AIzaSyDQF4CxmfbJg0HUURmVGfCX0NR6sSbGsOo',
+  googleProvider: '916587595257-8fqlt0henbsrp739bo6j2kpsh37rm9nb.apps.googleusercontent.com',
+  googleMapScript: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDQF4CxmfbJg0HUURmVGfCX0NR6sSbGsOo&libraries=places&callback=initMap&solution_channel=GMP_QB_addressselection_v1_cABC',
+  recaptcha:'6LePsSYpAAAAAPMwLtvMEJ_7ove_uFa3zSEt0YDw'
+};
